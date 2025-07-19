3325d18ac45ab4852b9480cc1d33e04e02fb1d82
stepanhampl <stepanhampl1@seznam.cz>
2025-07-19 20:46:55 +0200

    Refactor domain models and repository: replace domain classes with model classes, add Lombok annotations, and update VerseRepository imports.

--- backend/src/main/java/stepanhampl/bible/repository/VerseRepository.java
+++ backend/src/main/java/stepanhampl/bible/repository/VerseRepository.java
@@ -3,8 +3,9 @@ package stepanhampl.bible.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import stepanhampl.bible.domain.Verse;
+
 import stepanhampl.bible.dto.RandomVerseDto;
+import stepanhampl.bible.model.Verse;
 
 import java.util.List;
 
